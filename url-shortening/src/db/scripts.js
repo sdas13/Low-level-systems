958b9dd12f9e52c01d0b03e6573407d268ad4ad0
sdas13 <s.das001@yahoo.com>
2020-11-16 23:44:59 +0530

    offline keys collection

--- url-shortening/src/db/scripts.js
+++ url-shortening/src/db/scripts.js
@@ -1,31 +1,45 @@
 // Mongodb schema and validations
 
-use urlshorteningdb;
+// use urlshorteningdb;
 
-db.createCollection('URL',{
-    validator:{ 
-        $jsonSchema : {
-            bsonType:"object",
-            required : ["Hash", "OriginalURL"],
-            properties :{
-                Hash : {
-                    bsonType:"string",
-                    description:"Hash is required and must be a string"
-                },
-                OriginalURL:{
-                    bsonType:"string",
-                    description:"OriginalURL is required and must be a string"
-                },
-                CreationDate:{
-                    bsonType:"date",
-                    description:"CreationDate must be a date"
-                },
-                ExpirationDate:{
-                    bsonType:"date",
-                    description:"ExpirationDate must be a date"
-                }
-            }
-        }
+db.createCollection('URL', {
+  validator: {
+    $jsonSchema: {
+      bsonType: 'object',
+      required: ['Hash', 'OriginalURL'],
+      properties: {
+        Hash: {
+          bsonType: 'string',
+          description: 'Hash is required and must be a string',
+        },
+        OriginalURL: {
+          bsonType: 'string',
+          description: 'OriginalURL is required and must be a string',
+        },
+        CreationDate: {
+          bsonType: 'date',
+          description: 'CreationDate must be a date',
+        },
+        ExpirationDate: {
+          bsonType: 'date',
+          description: 'ExpirationDate must be a date',
+        },
+      },
+    },
+  },
+});
 
-    }
-})
+db.createCollection('OFFLINE-KEYS', {
+  validator: {
+    $jsonSchema: {
+      bsonType: 'object',
+      required: ['Key'],
+      properties: {
+        Key: {
+          bsonType: 'string',
+          description: 'Key must be a string',
+        },
+      },
+    },
+  },
+});
