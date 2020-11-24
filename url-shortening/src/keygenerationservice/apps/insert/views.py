bb1115412044afb571e6dcbdf6568f20f68c1c97
sdas13 <s.das001@yahoo.com>
2020-11-24 20:56:45 +0530

    keys generator

--- url-shortening/src/keygenerationservice/apps/insert/views.py
+++ url-shortening/src/keygenerationservice/apps/insert/views.py
@@ -5,10 +5,10 @@ from apps.insert import OfflineKeys
 def insert_keys():
 
     key_list = []
-    for i in range(50000):
+    for i in range(10000):
         rand_key = "".join(random.choices(string.ascii_letters + string.digits, k=6))
         key_list.append(OfflineKeys(key=rand_key))
 
     OfflineKeys.objects.insert(key_list)
-    # return OfflineKeys.objects.exclude("id").limit(10000).to_json()
+    # return OfflineKeys.objects.exclude("id").limit(100).to_json()
     return str(OfflineKeys.objects.count())
