10c933ffecb6f9329408ca6c967f536831c466cf
sdas13 <s.das001@yahoo.com>
2020-12-07 20:15:05 +0530

    continue inserting keys if error

--- url-shortening/src/keygenerationservice/apps/insert/views.py
+++ url-shortening/src/keygenerationservice/apps/insert/views.py
@@ -4,11 +4,22 @@ from apps.insert import OfflineKeys
 
 def insert_keys():
 
-    key_list = []
-    for i in range(100000):
-        rand_key = "".join(random.choices(string.ascii_letters + string.digits, k=6))
-        key_list.append(OfflineKeys(key=rand_key))
+    print("Request: Offline keys to be inserted...")
+    i = 0
+    while i < 500000:
+        try:
+            rand_key = "".join(
+                random.choices(string.ascii_letters + string.digits, k=6)
+            )
+            # key_list.append(OfflineKeys(key=rand_key))
+            OfflineKeys(key=rand_key).save()
+            i += 1
+        except Exception as e:
+            print("Exception ", e)
+            continue
 
-    OfflineKeys.objects.insert(key_list)
+    print("Done...")
+
+    # OfflineKeys.objects.insert(key_list)
     # return OfflineKeys.objects.exclude("id").limit(100).to_json()
     return str(OfflineKeys.objects.count())
