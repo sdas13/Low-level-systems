2c02518b223e10f742044a3bc2766dd22cf9528e
sdas13 <s.das001@yahoo.com>
2020-12-16 19:14:15 +0530

    keygen service - time measured

--- url-shortening/src/keygenerationservice/apps/load/views.py
+++ url-shortening/src/keygenerationservice/apps/load/views.py
@@ -1,14 +1,17 @@
 from clients import redis_client
 from apps.insert import OfflineKeys
+import time
 
 
 def load_keys_into_cache():
     collection = OfflineKeys._get_collection()
 
-    data = collection.find({}, {"_id": 0}).limit(10000)
+    start_time = time.time()
+    data = collection.find({}, {"_id": 0}).limit(500000)
     offline_keys = list(map(lambda i: i["key"], data))
 
     collection.delete_many({"key": {"$in": offline_keys}})
     redis_client.lpush("offline_keys", *offline_keys)
 
+    print(round(time.time() - start_time, 2))
     return str(redis_client.llen("offline_keys"))
