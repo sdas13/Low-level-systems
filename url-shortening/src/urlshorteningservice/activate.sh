8e199922c451bf9c40016de996fa05687ec7306f
sdas13 <s.das001@yahoo.com>
2020-12-24 19:19:29 +0530

    Added gunicorn

--- url-shortening/src/urlshorteningservice/activate.sh
+++ url-shortening/src/urlshorteningservice/activate.sh
@@ -1,1 +1,6 @@
-./venv/bin/python3 run.py
+# ./venv/bin/python3 run.py
+source ./venv/bin/activate
+# export "FLASK_APP=run:init_app()"
+# flask run --port $1
+ulimit -n 2048
+gunicorn --workers 4 --worker-class eventlet --threads 2 -b 0.0.0.0:5000 --access-logfile log.log --error-logfile error.log --capture-output "run:init_app()"
